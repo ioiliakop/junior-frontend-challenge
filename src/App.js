6124a59db1bc1e0332f1b3653c8196c0fd4c9952
ioiliakop <io.iliakop@gmail.com>
2019-10-17 20:49:53 +0300

    Experimenting with banners

--- src/App.js
+++ src/App.js
@@ -71,33 +71,41 @@ class App extends Component {
     const resultsExist = this.state.results.length > 0;
 
     return (
-      <div className="container pt-5">
-        {/* <h1>User Language: {this.userLanguage}</h1>
-        <h2>Keyword: {this.state.keyword}</h2> */}
+      <div className="container-fluid pt-5">
         <div className="row">
-          {/* <div className="desktopBanner border border-dark d-block">Banner Space</div> */}
-          <div className="col mb-3">
-            <img src="/xe.png" alt="xe logo" className="mx-auto d-block"/>
-          </div>
-          <form className="col-12 col-md-9 col-lg-8 col-xl-9 mx-auto" onSubmit={this.handleSubmit}>
-            <div className="form-group mb-0">
-              <label htmlFor="inputText">What place are you looking for?</label>
-              <input type="text" className="form-control" id="inputText" value={this.state.keyword} onChange={this.handleChange}/>
-            </div>
-            {/* <label>
-              What place are you looking for?
-              <input type="text" value={this.state.keyword} onChange={this.handleChange} onSubmit={this.handleSubmit}/>
-            </label> */}
-            <section className={'results border border-dark px-2 py-1' + (!resultsExist ? ' d-none' : '')}>
-              {this.state.results.map((result, index) => <div className="text-truncate" key={index} onClick={this.handleResultClick}>{result}</div>)}
-            </section>
-            <div className="d-flex justify-content-lg-start justify-content-center my-4">
-              <input type="submit" className="btn btn-warning" value="Click to Search" disabled={!resultsExist}/>
-            </div>
-          </form>
+        <div className="desktopBannerSpace d-none d-lg-inline-block ml-3 col-2">
+          <img src="/banner_space_150x400.png" alt="desktop banner" className="border border-dark"/>
         </div>
-        <div className="tabletBannerSpace border border-dark d-none d-md-block d-lg-none mb-5 text-center p-4">
-            Banner Space
+        <div className="container col-10">
+          {/* <h1>User Language: {this.userLanguage}</h1>
+          <h2>Keyword: {this.state.keyword}</h2> */}
+          <div className="row">
+            {/* <div className="desktopBanner border border-dark d-block">Banner Space</div> */}
+            <div className="col mb-3">
+              <img src="/xe.png" alt="xe logo" className="mx-auto d-block d-lg-inline-block"/>
+            </div>
+            <form className="col-12 col-md-9 col-lg-8 col-xl-9 mx-auto" onSubmit={this.handleSubmit}>
+              <div className="form-group mb-0">
+                <label htmlFor="inputText">What place are you looking for?</label>
+                <input type="text" className="form-control" id="inputText" value={this.state.keyword} onChange={this.handleChange}/>
+              </div>
+              {/* <label>
+                What place are you looking for?
+                <input type="text" value={this.state.keyword} onChange={this.handleChange} onSubmit={this.handleSubmit}/>
+              </label> */}
+              <section className={'results border border-dark px-2 py-1' + (!resultsExist ? ' d-none' : '')}>
+                {this.state.results.map((result, index) => <div className="text-truncate" key={index} onClick={this.handleResultClick}>{result}</div>)}
+              </section>
+              <div className="d-flex justify-content-lg-start justify-content-center my-4">
+                <input type="submit" className="btn btn-warning" value="Click to Search" disabled={!resultsExist}/>
+              </div>
+            </form>
+          </div>
+          {/* <div className="tabletBannerSpace border border-dark d-none d-md-block d-lg-none mb-5 text-center p-4"> */}
+          <div className="tabletBannerSpace d-none d-md-block d-lg-none mb-5">
+            <img src="/banner_space_700x100.png" alt="tablet banner" className="border border-dark"/>
+          </div>
+        </div>
         </div>
       </div>
     );
