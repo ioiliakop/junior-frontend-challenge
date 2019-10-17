9e05d65bf70d0e645d17343c7f4bb18704f9a7a2
ioiliakop <io.iliakop@gmail.com>
2019-10-17 15:46:12 +0300

    Added tablet banner space

--- src/App.js
+++ src/App.js
@@ -61,16 +61,19 @@ class App extends Component {
   }
 
   render() {
-    const hasResults = this.state.results.length > 0;
+    const resultsExist = this.state.results.length > 0;
 
     return (
       <div className="container pt-5">
         {/* <h1>User Language: {this.state.userLanguage}</h1>
         <h2>Keyword: {this.state.keyword}</h2> */}
         <div className="row">
-          <div className="col"><img src="/xegr.jpg" alt="xe.gr logo" className="mx-auto d-block"/></div>
-          <form className="col-12 col-lg-8 col-xl-9 mx-auto" onSubmit={this.handleSubmit}>
-            <div className="form-group">
+          {/* <div className="desktopBanner border border-dark d-block">Banner Space</div> */}
+          <div className="col">
+            <img src="/xe.png" alt="xe logo" className="mx-auto d-block"/>
+          </div>
+          <form className="col-12 col-md-9 col-lg-8 col-xl-9 mx-auto" onSubmit={this.handleSubmit}>
+            <div className="form-group mb-0">
               <label htmlFor="inputText">What place are you looking for?</label>
               <input type="text" className="form-control" id="inputText" value={this.state.keyword} onChange={this.handleChange} onSubmit={this.handleSubmit}/>
             </div>
@@ -78,11 +81,16 @@ class App extends Component {
               What place are you looking for?
               <input type="text" value={this.state.keyword} onChange={this.handleChange} onSubmit={this.handleSubmit}/>
             </label> */}
-            <section className={'results border border-dark px-2 py-1 mb-3' + (!hasResults ? ' d-none' : '')}>
+            <section className={'results border border-dark px-2 py-1' + (!resultsExist ? ' d-none' : '')}>
             {this.state.results.map((result, index) => <div className="text-truncate" key={index} onClick={this.handleResultClick}>{result}</div>)}
             </section>
-            <div className="d-flex justify-content-xl-start justify-content-lg-start justify-content-center"><input type="submit" className="btn btn-warning" value="Click to Search" disabled={!hasResults}/></div>
+            <div className="d-flex justify-content-lg-start justify-content-center my-4">
+              <input type="submit" className="btn btn-warning" value="Click to Search" disabled={!resultsExist}/>
+            </div>
           </form>
+        </div>
+        <div className="tabletBannerSpace border border-dark d-none d-md-block d-lg-none mb-5 text-center p-4">
+            Banner Space
         </div>
       </div>
     );
