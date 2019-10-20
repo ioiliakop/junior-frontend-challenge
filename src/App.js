50a7ee2bb3f5cfd3642d31fd19c19910a1999a34
ioiliakop <io.iliakop@gmail.com>
2019-10-20 22:37:01 +0300

    Cleaned code

--- src/App.js
+++ src/App.js
@@ -1,40 +1,20 @@
 import React, { Component } from 'react';
 import { isMobile } from 'react-device-detect';
 
-function BoldifiedResult(props) {
-  var re = new RegExp(props.keyword, "gi");
-  const unmatchedText = props.result.split(re);
-  console.log('Split result:', unmatchedText);
-  let newResult = [];
-  let sum = -1;
-  for (let index = 0; index < (unmatchedText.length - 1); index++) {
-    newResult.push(<span key={index}>{unmatchedText[index]}</span>);
-    sum += unmatchedText[index].length;
-    newResult.push(<b key={index + 1000}>{props.result.substring(sum, props.keyword.length)}</b>);
-    console.log(unmatchedText[index].length, props.result.substring(sum, props.keyword.length));
-    sum += props.keyword.length;     
-  }
-  console.log('result:', props.result);
-  console.log('keyword:', props.keyword);
-  console.log(newResult);
-  return <span>{newResult}</span>;
-}
-
 class App extends Component {
 
   constructor(props) {
     super(props);
-    // We set in our state the input keyword and the results from our service, as we expect them to change
+    // We set in our state the input keyword and the results from our service, as we expect them to change often
     this.state = {
       keyword: '',
       results: [],
       errorMessage: ''
     }
+    this.fetchResults = this.fetchResults.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.fetchResults = this.fetchResults.bind(this);
     this.handleResultClick = this.handleResultClick.bind(this);
-    this.boldifyKeyword = this.boldifyKeyword.bind(this);
 
     // We assume browser language and device type remain the same for each App instance, therefore we calculate their values only once here
     // We set english as default, therefore only check for greek language
@@ -47,20 +27,10 @@ class App extends Component {
     this.timeoutID = null;
   }
 
-  boldifyKeyword(original) {
-    const substring = new RegExp(this.keyword, "gi");
-
-    original.replace(substring, (match) => {
-      return match.bold();  
-    });
-  }
-
   // Fetches the results for the input keyword, after constructing the appropriate url
   // Then sets the relative fields in the state
   fetchResults() {
     const url = 'http://35.180.182.8/Search?keywords=' + this.state.keyword + '&language=' + this.browserLanguage + '&limit=' + this.limit;
-
-    console.log('url: ', url);
 
     fetch(url, {
       method: 'GET',
@@ -72,8 +42,7 @@ class App extends Component {
                 results: data.entries.map(entry => entry.name),
                 errorMessage: ''
             });
-            console.log('data: ', this.state.results);           
-           })
+          })
         } else if (response.status >= 400) {
           this.setState({errorMessage: 'Error ' + response.status + ': ' + response.statusText});
           console.error(response);
@@ -82,25 +51,26 @@ class App extends Component {
   }
 
   // Handles changes in the input field
-  // First it clears the current timeoutID (if any)
-  // Then sets a new timeoutID for the current change
-  // If it doesn't get cleared by a new change in the time period we have set (delay) it will
+  // 1 - It clears the current timeoutID (if any)
+  // 2 - It sets updated keyword to state
+  // 3 - It then sets a new timeoutID for the current change
+  // If it doesn't get cleared by a new change in the time period we have set (delay), it will
   // either execute the fetchResults to get new results from the service
-  // or simply clear our results if we have less than 2 characters
+  // or simply clear our results if the keyword is less than 2 characters
   handleChange(event) {
     if (this.timeoutID) {
       clearTimeout(this.timeoutID);
     }
     this.setState({keyword: event.target.value
-      }, () => this.state.keyword.length > 1 ? 
-          this.timeoutID = setTimeout(this.fetchResults, this.delay)
+      }, () => this.state.keyword.length > 1
+        ? this.timeoutID = setTimeout(this.fetchResults, this.delay)
         : this.setState({results: [], errorMessage: ''}));
   }
 
   // Redirects to google.com making a search for the query
   handleSubmit(event) {
-    window.open('https://google.com/search?q=' + this.state.keyword); //in new tab
-    // window.location.assign('https://google.com/search?q=' + this.state.keyword); //in same tab alternatively
+    window.open('https://google.com/search?q=' + this.state.keyword); // in new tab
+    // window.location.assign('https://google.com/search?q=' + this.state.keyword); // in same tab alternatively
     event.preventDefault();
   }
 
@@ -109,8 +79,6 @@ class App extends Component {
   // 2 - It adds background color to the selected result, thus making it standout (appear as selected)
   // 3 - It sets it as the keyword in the state of our app
   handleResultClick(event) {
-    console.log('selected result:', event.target.innerText);
-    console.log(event.target);
     event.target.parentElement.childNodes.forEach(child => child.classList.remove('bg-warning'));
     event.target.classList.add('bg-warning');
     this.setState({keyword: event.target.innerText});
@@ -122,19 +90,14 @@ class App extends Component {
     return (
       <div className="container-fluid pt-3 pt-md-4">
         <div className="row">
-          {/* <div className="desktopBannerSpace d-none d-lg-inline-block ml-3 col-1">
-            <img src="/banner_space_150x400.png" alt="desktop banner" className="border border-dark"/>
-          </div> */}
-          {/* Left vertical banner visible in desktop */}
+          {/* Left vertical banner visible only in desktop (large (lg) and extra large (xl) viewports) */}
           <div className="d-none d-lg-block ml-3 col-lg-3 col-xl-2">
             <img src="/banner_space_150x400.png" alt="desktop banner" className="border border-dark" height="400" width="150"/>
-          {/* <img src="/banner_space_150x400.png" alt="desktop banner" className="d-none d-lg-inline-block ml-5 col-1 border border-dark" height="400" width="150"/> */}
           </div>
+          
+          {/* Main container of our app, includes all other elements */}
           <div className="container col-lg-9 ml-lg-n4">
-            {/* <h1>User Language: {this.browserLanguage}</h1>
-            <h2>Keyword: {this.state.keyword}</h2> */}
             <div className="row">
-              {/* <div className="desktopBanner border border-dark d-block">Banner Space</div> */}
               <div className="col-lg-2 mb-3 ml-lg-n5 ml-xl-n3 ">
                 <img src="/xe.png" alt="xe logo" className="mx-auto d-block" width="130"/>
               </div>
@@ -145,28 +108,28 @@ class App extends Component {
                   <label htmlFor="inputText">What place are you looking for?</label>
                   <input type="text" className="form-control" id="inputText" value={this.state.keyword} onChange={this.handleChange}/>
                 </div>
+
                 {/* Results Section */}
-                <div className={'border border-dark px-2 py-1' + (!resultsExist ? ' d-none' : '')} id="results">
-                  {/* {this.state.results.map((result, index) => <div className="text-truncate" key={index} onClick={this.handleResultClick}><BoldifiedResult result={result} keyword={this.state.keyword}/></div>)} */}
+                <div className={'border border-dark px-2 py-1' + (!resultsExist ? ' d-none' : '')}>
                   {this.state.results.map((result, index) => <div className="text-truncate" key={index} onClick={this.handleResultClick}>{result}</div>)}
                 </div>
+
                 {/* Form Search Button */}
                 <div className="d-flex justify-content-lg-start justify-content-center my-4">
                   <input type="submit" className="btn btn-warning" value="Click to Search" disabled={!resultsExist}/>
                 </div>
+                
                 {/* Error Message for the user should it occur, while connecting to the back-end service */}
                 {this.state.errorMessage 
-                ? <div className="text-center text-danger my-4"><p>An error has occured</p>{this.state.errorMessage}</div> 
+                ? <div className="text-center text-danger my-4">An error has occured<br/>{this.state.errorMessage}</div> 
                 : null}
               </form>
-
             </div>
-            {/* <div className="tabletBannerSpace border border-dark d-none d-md-block d-lg-none mb-5 text-center p-4"> */}
-            {/* <div className="tabletBannerSpace d-none d-md-block d-lg-none mb-5"> */}
-            {/* Bottom horizontal banner visible in tablet mode */}
+
+            {/* Bottom horizontal banner visible only in tablet mode (medium (md) viewport) */}
             <img src="/banner_space_700x100.png" alt="tablet banner" className="img-fluid d-none d-md-block d-lg-none border border-dark mb-4"/>
-            {/* </div> */}
           </div>
+
         </div>
       </div>
     );
